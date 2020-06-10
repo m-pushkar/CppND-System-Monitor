71afb147db85c8c343173e959bfeb40633d5ad33
Pushkar Mehendale <mehenpm@gmail.com>
2020-06-10 16:43:07 -0500

    refactor: created format.cpp code

--- src/format.cpp
+++ src/format.cpp
@@ -1,11 +1,32 @@
-#include <string>
-
 #include "format.h"
 
+#include <iostream>
+#include <string>
+
 using std::string;
+using std::stringstream;
 
 // TODO: Complete this helper function
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
 // REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds[[maybe_unused]]) { return string(); }
+string Format::ElapsedTime(long seconds) {
+  // Converts system recorded seconds to H:M:S
+  long hour, min, sec;
+  hour = seconds / 3600;
+  min = (seconds % 3600) / 60;
+  sec = seconds % 60;
+
+  // Display converted output
+  stringstream output;
+  output.fill('0');
+  output.width(2);
+  output << hour << ":";
+  output.fill('0');
+  output.width(2);
+  output << min << ":";
+  output.fill('0');
+  output.width(2);
+  output << sec;
+  return output.str();
+}
